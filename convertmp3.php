d203add8271dd620405a48506f790ba1f1c9265d
Apjal-ru <mashaqqi18@gmail.com>
2023-11-24 02:50:35 +0700

    converter update

--- convertmp3.php
+++ convertmp3.php
@@ -2,20 +2,19 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["videoFile"])) {
     $inputVideo = $_FILES["videoFile"]["tmp_name"];
-    $outputMP3 = 'output/audio.mp3';
+    $outputFile = 'output';
 
-    if (convertVideoToMP3($inputVideo, $outputMP3)) {
+    if (convertToMP3($inputVideo, $outputFile)) {
         echo "Konversi video ke MP3 berhasil!";
     } else {
         echo "Konversi video ke MP3 gagal.";
     }
 }
 
-function convertVideoToMP3($inputVideo, $outputMP3) {
+function convertToMP3($inputVideo, $outputFile) {
     $ffmpegPath = 'ffmpeg';
-    $command = "$ffmpegPath -i $inputVideo -q:a 0 -map a $outputMP3";
+    $command = "$ffmpegPath -i $inputVideo -q:a 0 -map a $outputFile";
     exec($command, $output, $returnCode);
-
     return $returnCode === 0;
 }
 
